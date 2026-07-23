095fe19bea83cd5dbde2fc22442b328176fb2ad9
DanielWisn <146425555+DanielWisn@users.noreply.github.com>
2026-07-23 12:38:05 +0200

    Kafka services health endpoint

--- payment-service/src/main/java/com/paymentservice/controller/HealthController.java
+++ payment-service/src/main/java/com/paymentservice/controller/HealthController.java
@@ -1,5 +1,7 @@
-package com.userservice.controllers;
+package com.paymentservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/health")
 public class HealthController {
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
     @GetMapping
     public String health() {
+        kafkaTemplate.send("test-topic","hello from payment-service");
         return "OK";
     }
 }
--- shopping-cart-service/src/main/java/com/shoppingcartservice/controller/HealthController.java
+++ shopping-cart-service/src/main/java/com/shoppingcartservice/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.shoppingcartservice.controllers;
+package com.shoppingcartservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
